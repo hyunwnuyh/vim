8959d43986ca3155d6eade6efc5ffd7165746caf
hyunwnuyh <hyunwoo2846@gmail.com>
2019-05-14 15:30:46 +0900

    2019.05.14

--- getcurr.sh
+++ getcurr.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
+rm -rfv .vim
+rm -fv .vimrc
+mkdir .vim
 cp -rfv ~/.vim/* .vim
 cp -fv ~/.vimrc .vimrc
--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ ln -sfv ~/.vimrc ~/.config/nvim/init.vim
 ln -sfv ~/.vim/colors/ ~/.config/nvim/colors/
 chmod -R 777 ~/.vim
 chmod 777 ~/.vimrc
+rm -r ~/.vim/bundle/*
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "Install completed"
